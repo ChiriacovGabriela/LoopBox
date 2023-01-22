1d1b5315afae9a26a8977f4e3200c91fe1489f02
AndraCarla <113671176+AndraCarla@users.noreply.github.com>
2023-01-22 17:13:54 +0100

    front v2 create add playlist

---
+++ src/Controller/PlaylistController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Playlist;
+use App\Form\PlaylistFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlaylistController extends AbstractController
+{
+    #[Route('/playlist', name: 'app_playlist')]
+    public function index(): Response
+    {
+        return $this->render('playlist/index.html.twig', [
+            'controller_name' => 'PlaylistController',
+        ]);
+    }
+
+    #[Route('/playlist/add', name: 'add')]
+    public function add(): Response
+    {
+        //On crée un nouveau Playlist
+        $playlist = new Playlist();
+        //On crée le formulaire
+        $playlistForm = $this->createForm(PlaylistFormType::class, $playlist);
+        return $this->render('playlist/add.html.twig', [
+            'playlistForm' => $playlistForm->createView()
+        ]);
+    }
+}
