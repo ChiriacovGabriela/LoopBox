f6a7173d79305b2a6dbcc249a50d66f8d08ef8ba
AndraCarla <113671176+AndraCarla@users.noreply.github.com>
2023-01-26 16:38:46 +0100

    modif

--- src/Controller/PlaylistController.php
+++ src/Controller/PlaylistController.php
@@ -19,15 +19,58 @@ class PlaylistController extends AbstractController
     }
 
     #[Route('/playlist/add', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $em): Response
     {
         //On crée un nouveau Playlist
         $playlist = new Playlist();
         //On crée le formulaire
         $playlistForm = $this->createForm(PlaylistFormType::class, $playlist);
+        // On traite la requete du formulaire
+        $playlistForm->handleRequest($request);
+
+        // on verifie si le formulaire est soumis et valide
+        if($playlistForm->isSubmitted() && $playlistForm->isValid()){
+            //On stock
+            $em-> persist($playlist);
+            $em->flush();
+
+
+            //On redirige
+            return $this->redirectToRoute('app_playlist');
+
+        }
+
         return $this->render('playlist/add.html.twig', [
             'playlistForm' => $playlistForm->createView()
         ]);
     }
+    #[Route('/playlist/edit/{id}', name: 'edit')]
+    public function edit (Playlist $playlist, Request $request, EntityManagerInterface $em ):Response
+    {
+        // ajouter la date pour update
+        $playlist ->setUpdated_at(new \DateTimeImmutable());
+        //On crée le formulaire
+        $playlistForm = $this->createForm(PlaylistFormType::class, $playlist);
+        // On traite la requete du formulaire
+        $playlistForm->handleRequest($request);
+        // on verifie si le formulaire est soumis et valide
+        if($playlistForm->isSubmitted() && $playlistForm->isValid()){
+            //On stock
+            $em-> persist($playlist);
+            $em->flush();
+
+
+            //On redirige
+            return $this->redirectToRoute('app_playlist');
+
+        }
+        return $this->render('playlist/edit.html.twig', [
+            'playlistForm' => $playlistForm->createView(),
+            'playlist' => $playlist
+        ]);
+
+    }
+
+
 
 }
