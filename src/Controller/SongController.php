ff99d0803005147ced97839dfc56ccdbaaefede3
Yracerlyn <yessicerlyn@gmail.com>
2023-01-27 15:55:52 +0100

    Success upload audio.midi

---
+++ src/Controller/SongController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Song;
+use App\Form\SongType;
+use App\Repository\SongRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+#[Route('/song')]
+class SongController extends AbstractController
+{
+    #[Route('/', name: 'app_song_index', methods: ['GET'])]
+    public function index(SongRepository $songRepository): Response
+    {
+        return $this->render('song/index.html.twig', [
+            'songs' => $songRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/new', name: 'app_song_new', methods: ['GET', 'POST'])]
+    public function new(Request $request,EntityManagerInterface $em ,SluggerInterface $slugger): Response
+    {
+        $song = new Song();
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $songFile*/
+            $songFile = $form->get('audioPath')->getData();
+            if ($songFile){
+                $originalFilename = pathinfo($songFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$songFile->guessExtension();
+
+                try {
+                    $songFile->move(
+                        $this->getParameter('song_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $song->setAudioPath($newFilename);
+            }
+
+            $em-> persist($song);
+            $em->flush();
+
+            return $this->redirectToRoute('app_song_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('song/new.html.twig', [
+            'song' => $song,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_song_show', methods: ['GET'])]
+    public function show(Song $song): Response
+    {
+        return $this->render('song/show.html.twig', [
+            'song' => $song,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_song_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Song $song, SongRepository $songRepository): Response
+    {
+        $song -> setUpdatedAt(new \DateTimeImmutable());
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $songRepository->save($song, true);
+
+            return $this->redirectToRoute('app_song_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('song/edit.html.twig', [
+            'song' => $song,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_song_delete', methods: ['POST'])]
+    public function delete(Request $request, Song $song, SongRepository $songRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$song->getId(), $request->request->get('_token'))) {
+            $songRepository->remove($song, true);
+        }
+
+        return $this->redirectToRoute('app_song_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
