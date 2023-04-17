c1e0cc91715a36d8bceb49f9fa96ddbaa043f3e2
Yracerlyn <yessicerlyn@gmail.com>
2023-04-17 22:29:21 +0200

    ajout de musique + liste déroulante pour l'album

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -16,7 +17,15 @@ class AlbumType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('type')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Rap' => 'rap',
+                    'Pop' => 'pop',
+                    'Metal' => 'metal',
+                    'Rock' => 'rock',
+                    'Classical Music' => 'classical music',
+                    'Jazz' => 'jazz',
+                ]])
             ->add('artist')
             ->add('pictureFileName',FileType::class,[
                 'label' => 'Image',
